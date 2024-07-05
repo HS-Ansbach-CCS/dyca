3cef50c61d298cc5de9de3b01df702dc15a1fc5b
annikastiehl <annika.stiehl@hs-ansbach.de>
2024-07-05 10:24:59 +0200

    update

--- setup.py
+++ setup.py
@@ -2,22 +2,26 @@ from setuptools import setup
 
 setup(
     name='dyca',
-    version='0.1.0',
+    version='0.1.1',
     description='Dynamical Component Analysis - A method to decompose multivariate signals',
+    url='https://github.com/HS-Ansbach-CCS/dyca',
     author='Annika Stiehl',
     author_email='annika.stiehl@hs-ansbach.de',
     license='GPL-3.0',
     packages=['dyca'],
-    install_requires=['numpy==1.26.4', 'matplotlib==3.8.4', 'scipy==1.13.0'],
+    install_requires=['numpy>=1.18.0,<2.0.0', 
+                      'matplotlib', 
+                      'scipy==1.13.0'],
+
+    python_requires='>=3.10',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering :: Signal Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Signal Decomposition',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics'
     ],
 
 )
